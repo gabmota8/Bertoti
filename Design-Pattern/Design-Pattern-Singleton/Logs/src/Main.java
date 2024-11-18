1c437617fc86bfec6533b48ab6f6e50ebeaf24a3
gabmota8 <gabmota8@hotmail.com>
2024-11-18 20:34:29 -0300

    first commit

---
+++ Design-Pattern/Design-Pattern-Singleton/Logs/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("Iniciando a aplicação...");
+        // ...
+        logger.log("Aplicação encerrada.");
+    }
+}
